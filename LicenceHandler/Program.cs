0ff7ca47cc3ade51425459047b3d1938d085df46
zsombor9 <zsombor90@gmail.com>
2016-01-04 17:05:17 +0100

    initial commit

---
+++ LicenceHandler/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.IO;
+
+
+namespace LicenceHandler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                string fileToEncrypt = @"C: \Users\Zsombor\Documents\Visual Studio 2015\Projects\LicenceHandler\docs\LICENSE";
+                string fileToDecrypt = @"C:\Users\Zsombor\Documents\Visual Studio 2015\Projects\LicenceHandler\Encrypt\LICENSE.enc";
+                string key = "palmon";
+
+
+                RSAProvider rsap = new RSAProvider(key);
+               rsap.encryptUsingPublicKey(fileToEncrypt);
+               rsap.decryptFileUsingPrivateKey(fileToDecrypt, "palmon");             
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
